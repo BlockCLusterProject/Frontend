d99a3a7d01f1ba34ca032024ed2c89ab9abba461
Juan Camilo Ramirez <jcra.juancamilo@gmail.com>
2025-02-21 19:23:23 -0500

    Se agegan archvos faltantes

---
+++ src/modelos/VentaPelicula.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Dell
+ */
+public class VentaPelicula {
+    private ArrayList<Pelicula> peliculas;
+    private boolean active;
+    
+    public VentaPelicula(ArrayList<Pelicula> peliculas, boolean active){
+        this.peliculas = peliculas;
+        this.active = active;
+    }   
+    
+    public VentaPelicula(){
+        
+    }
+}
